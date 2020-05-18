c36a71e8f07580bb4e9aaa74aacba5fd5a338301
gigoallen <gigo@hk.edu.tw>
2020-05-18 11:39:17 +0800

    完成專案

--- app/src/main/java/com/gigoallen/intentbundle0518/BmiActivity.java
+++ app/src/main/java/com/gigoallen/intentbundle0518/BmiActivity.java
@@ -2,6 +2,8 @@ package com.gigoallen.intentbundle0518;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class BmiActivity extends AppCompatActivity {
 
@@ -9,5 +11,41 @@ public class BmiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+
+        CalBmi();
     }
+
+    private void CalBmi() {
+
+        double height, weight, bmi;
+
+        Bundle bundle = this.getIntent().getExtras();
+
+        if(bundle != null){
+
+           height = Double.parseDouble(bundle.getString("HEIGHT"));
+           weight = Double.parseDouble(bundle.getString("WEIGHT"));
+
+           height = height/100;
+
+           bmi = weight/(height*height);
+
+           String result = "正常";
+
+           if(bmi<35 && bmi>30)
+               result = "中度肥胖";
+           else if(bmi<30 && bmi>27)
+               result = "輕度肥胖";
+
+            TextView txvBMI = findViewById(R.id.txvBMI);
+
+            txvBMI.setText("你的BMI: " + bmi + "\n" + result);
+
+        }
+    }
+
+    public void btnOK(View view) {
+        finish();
+    }
+
 }
--- app/src/main/java/com/gigoallen/intentbundle0518/MainActivity.java
+++ app/src/main/java/com/gigoallen/intentbundle0518/MainActivity.java
@@ -1,13 +1,40 @@
 package com.gigoallen.intentbundle0518;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtHeight, edtWeight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find R
+        edtHeight = findViewById(R.id.edtHeight);
+        edtWeight = findViewById(R.id.edtWeight);
+
+    }
+
+    public void btnBMI(View view) {
+
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, BmiActivity.class);
+//        Intent intent = new Intent(MainActivity.this, BmiActivity.class);
+
+        Bundle bundle = new Bundle();
+
+        bundle.putString("HEIGHT", edtHeight.getText().toString());
+        bundle.putString("WEIGHT", edtWeight.getText().toString());
+
+        intent.putExtras(bundle);
+        startActivity(intent);
+
     }
 }
